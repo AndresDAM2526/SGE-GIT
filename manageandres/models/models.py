ba4e53283941712002a3fa3ec5cb2ab87c8c5a0f
andrescg01 <andrescg2001@gmail.com>
2025-11-22 19:21:39 +0100

    Se termina practica 9

--- manageandres/models/models.py
+++ manageandres/models/models.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
-
-# class manageandres(models.Model):
-#     _name = 'manageandres.manageandres'
-#     _description = 'manageandres.manageandres'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
 
 class task(models.Model):
     _name='manageandres.task'
@@ -23,9 +8,13 @@ class task(models.Model):
 
     name=fields.Char()
     description=fields.Char()
-    star_date=fields.Datetime()
+    start_date=fields.Datetime()
     end_date=fields.Datetime()
     is_paused=fields.Boolean()
+
+    history_id=fields.Many2one("manageandres.history",string="History", required=True,ondelete="cascade")
+    sprint_id=fields.Many2one("manageandres.sprint",string="Sprint",required=True,ondelete="cascade")
+    technologies_id=fields.Many2many(comodel_name="manageandres.technology",relation="sprint_task",column1="technology_id",column2="task_id",string="Technologies")
 
 
 class sprint(models.Model):
@@ -37,12 +26,21 @@ class sprint(models.Model):
     start_date=fields.Datetime()
     end_date=fields.Datetime()
 
+    tasks_id=fields.One2many(string="Tasks",comodel_name="manageandres.task",inverse_name="sprint_id")
+    project_id=fields.Many2one("manageandres.project",string="Project",required=True,ondelete="cascade")
+    
+
 class project(models.Model):
     _name='manageandres.project'
     _description='manageandres.project'
 
     name=fields.Char()
     description=fields.Char()
+
+    histories_id=fields.One2many(string="Histories",comodel_name="manageandres.history",inverse_name="project_id")
+    sprints_id=fields.One2many(string="Sprints",comodel_name="manageandres.sprint",inverse_name="project_id")
+
+
 
 class history(models.Model):
     _name='manageandres.history'
@@ -51,6 +49,9 @@ class history(models.Model):
     name=fields.Char()
     description=fields.Char()
 
+    project_id=fields.Many2one("manageandres.project",string="Project",required=True,ondelete="cascade")
+    tasks_id=fields.One2many(string="Tasks",comodel_name="manageandres.task",inverse_name="history_id")
+
 class technology(models.Model):
     _name='manageandres.technology'
     _description='manageandres.technology'
@@ -58,3 +59,5 @@ class technology(models.Model):
     name=fields.Char()
     description=fields.Char()
     photo=fields.Image()
+
+    tasks_id=fields.Many2many(comodel_name="manageandres.task",relation="sprint_task",column1="task_id",column2="technology_id",string="Tasks")
