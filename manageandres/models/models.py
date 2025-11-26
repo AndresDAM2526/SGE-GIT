b5859f30b0a96afce975cc325fd755eebe2636bc
andrescg01 <andrescg2001@gmail.com>
2025-11-26 19:58:49 +0100

    Punto 6 finalizado

--- manageandres/models/models.py
+++ manageandres/models/models.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+import datetime
 
 
 class task(models.Model):
     _name='manageandres.task'
     _description='manageandres.task'
 
+    project=fields.Many2one("manageandres.project",related="history_id.project_id",readonly=True)
+    code=fields.Char(compute="_get_code", store=False)
     name=fields.Char()
     description=fields.Char()
     start_date=fields.Datetime()
@@ -13,9 +16,26 @@ class task(models.Model):
     is_paused=fields.Boolean()
 
     history_id=fields.Many2one("manageandres.history",string="History", required=True,ondelete="cascade")
-    sprint_id=fields.Many2one("manageandres.sprint",string="Sprint",required=True,ondelete="cascade")
+    sprint=fields.Many2one("manageandres.sprint",compute="_get_sprint",store=True)
     technologies_id=fields.Many2many(comodel_name="manageandres.technology",relation="sprint_task",column1="technology_id",column2="task_id",string="Technologies")
 
+    def _get_code(self):
+        for task in self:
+            task.code="TSK_"+str(task.id)
+    
+    @api.depends('code')
+    def _get_sprint(self):
+        for task in self:
+            sprints=self.env['manageandres.sprint'].search([('project_id','=',task.history_id.project_id.id)])
+            found=False
+            for sprint in sprints:
+                if isinstance(sprint.end_date,datetime.datetime) and sprint.end_date>datetime.datetime.now():
+                    task.sprint=sprint.id
+                    found=True
+            if not found:
+                task.sprint=False
+
+    
 
 class sprint(models.Model):
     _name='manageandres.sprint'
@@ -24,10 +44,20 @@ class sprint(models.Model):
     name=fields.Char()
     description=fields.Char()
     start_date=fields.Datetime()
-    end_date=fields.Datetime()
+    duration=fields.Integer()
+    end_date=fields.Datetime(compute="_get_end_date",store=True)
+    
 
-    tasks_id=fields.One2many(string="Tasks",comodel_name="manageandres.task",inverse_name="sprint_id")
+    tasks_id=fields.One2many(string="Tasks",comodel_name="manageandres.task",inverse_name="sprint")
     project_id=fields.Many2one("manageandres.project",string="Project",required=True,ondelete="cascade")
+
+    @api.depends('start_date','duration')
+    def _get_end_date(self):
+        for sprint in self:
+            if isinstance(sprint.start_date,datetime.datetime) and sprint.duration>0:
+                sprint.end_date=sprint.start_date+datetime.timedelta(days=sprint.duration)
+            else:
+                sprint.end_date=sprint.start_date
     
 
 class project(models.Model):
@@ -51,6 +81,18 @@ class history(models.Model):
 
     project_id=fields.Many2one("manageandres.project",string="Project",required=True,ondelete="cascade")
     tasks_id=fields.One2many(string="Tasks",comodel_name="manageandres.task",inverse_name="history_id")
+    used_technologies=fields.Many2many("manageandres.technology",compute="_get_used_technologies")
+
+    def _get_used_technologies(self):
+        for history in self:
+            technologies=None
+            for task in history.tasks_id:
+                if not technologies:
+                    technologies=task.technologies_id
+                else:
+                    technologies=technologies+task.technologies_id
+            history.used_technologies=technologies
+
 
 class technology(models.Model):
     _name='manageandres.technology'
